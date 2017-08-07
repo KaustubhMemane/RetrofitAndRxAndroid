0aff8b2a42bbd05bebabaaf25d7aa4523bf01016
Kaustubh Memane <memane.kaustubh@gmail.com>
2017-08-06 21:00:43 -0400

    Update 3
    Final Clean code

--- app/src/main/java/com/kmema/android/retrofitandrxandroid/MainActivity.java
+++ app/src/main/java/com/kmema/android/retrofitandrxandroid/MainActivity.java
@@ -1,4 +1,5 @@
 package com.kmema.android.retrofitandrxandroid;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -6,9 +7,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
-
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
 
 import java.io.IOException;
 import java.util.concurrent.Callable;
@@ -20,18 +18,14 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-    private static final  String TAG = "MainActivity_TAG:";
-   /* Observable<ResumeDataType> mobservable;
-    Observer<ResumeDataType> mObserver;
-   */ TextView mTextView;
-    Subscription subscription;
+    private static final String TAG = "MainActivity_TAG:";
+    private TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,18 +33,18 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.tvHello);
         mTextView = (TextView) findViewById(R.id.tvHello);
 
-         Observable.defer(new Callable<ObservableSource<ResumeDataType>>() {
-                    @Override
-                    public ObservableSource<ResumeDataType> call() throws Exception {
+        Observable.defer(new Callable<ObservableSource<ResumeDataType>>() {
+            @Override
+            public ObservableSource<ResumeDataType> call() throws Exception {
 
-                        ResumeDataType resumeDataType=getDataFromJson();
+                ResumeDataType resumeDataType = getDataFromJson();
 
-                        return (Observable.just(resumeDataType));
-                    }
-                })
+                return (Observable.just(resumeDataType));
+            }
+        })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                 .subscribe(new Observer<ResumeDataType>() {
+                .subscribe(new Observer<ResumeDataType>() {
                     @Override
                     public void onSubscribe(@NonNull Disposable d) {
 
@@ -58,13 +52,13 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onNext(@NonNull ResumeDataType resumeDataType) {
-                        Toast.makeText(MainActivity.this, resumeDataType.className+"Hope", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, resumeDataType.className + "Hope", Toast.LENGTH_SHORT).show();
                         mTextView.setText(resumeDataType.overview);
                     }
 
                     @Override
                     public void onError(@NonNull Throwable e) {
-                        Log.e(TAG,e.getMessage());
+                        Log.e(TAG, e.getMessage());
                         Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
                     }
 
@@ -77,21 +71,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @SuppressWarnings("ConstantConditions")
     private ResumeDataType getDataFromJson() throws IOException {
-        ResumeDataType resumeData = new ResumeDataType();
+        ResumeDataType resumeData;
         final OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url("http://kaustubhmemane.com/my_resume_overview")
                 .build();
         Response response = client.newCall(request).execute();
-        
         Gson gson = new Gson();
-        ResumeDataType r = gson.fromJson(response.body().string(),ResumeDataType.class);
-        return r;
+        resumeData = gson.fromJson(response.body().string(), ResumeDataType.class);
+        return resumeData;
     }
 
-    public Observable<ResumeDataType> resumeDataTypeObservable()
-    {
-        return null;
-    }
 }
